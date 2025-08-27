d41d8683dfeac1582e4acd7f67a3099ca8c432f5
Malsha Gamage <malshagamage137@gmail.com>
2025-08-27 23:06:53 +0530

    Revert "Implement BBVAS APIs"
    This reverts commit c240ddaf19723fa2dcc91eaedb073558b81484ee.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
-const promotionRoutes = require("./BBVAS/BonusData/routes/promotionRoutes");
-const usageRoutes = require("./BBVAS/UssageSummery/routes/usageRoutes");
-const dataGiftRoutes = require("./BBVAS/GiftDatapackages/routes/dataGiftRoutes");
-const voucherRoutes = require("./BBVAS/RedeemVoucher/routes/voucherRoutes");
-const dataTransferRoutes = require("./BBVAS/GetDataTransferAmounts/routes/dataTransferRoutes");
-const advancedReportRoutes = require("./BBVAS/PurchaseAdvancedReports-Postpaid/routes/advancedReportRoutes");
+const promotionRoutes = require('./BBVAS/BonusData/routes/promotionRoutes');
 // const accountRoutes = require('./routes/account.routes');
 
 // Middleware
@@ -14,17 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/tmf-api/promotionManagement/v4/promotion", promotionRoutes);
-app.use("/tmf-api/usageManagement/v4/Usage", usageRoutes);
-app.use("/tmf-api/usageManagement/v4/DataGiftPackages", dataGiftRoutes);
-app.use("/tmf-api/usageManagement/v4/Vouchers", voucherRoutes);
-app.use("/tmf-api/usageManagement/v4/DataTransferAmounts", dataTransferRoutes);
-app.use("/tmf-api/usageManagement/v4/AdvancedReports", advancedReportRoutes);
+app.use('/tmf-api/promotionManagement/v4/promotion', promotionRoutes);
 // app.use('/api/Account', accountRoutes);
 
 // Health check
-app.get("/", (req, res) => {
-  res.send("Omini API Server is running ✅");
+app.get('/', (req, res) => {
+  res.send('Omini API Server is running ✅');
 });
 
 module.exports = app;
