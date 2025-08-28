41d72f15f834bdbed8e8ce7d6b59794b606bef37
Irosha Geethanjalie <geethaanjaleeci@gmail.com>
2025-08-28 11:08:19 +0530

    create jwt token

--- src/BBVAS/ChangeBBPassword/routes/customerRoutes.js
+++ src/BBVAS/ChangeBBPassword/routes/customerRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { changeBBPassword } = require("../controllers/customerController");
+const auth = require("../middleware/authMiddleware");
 
+
+router.put("/:customerId/changeBBPassword", auth, changeBBPassword);
 router.put("/:customerId/changeBBPassword", changeBBPassword);
 
 module.exports = router;
