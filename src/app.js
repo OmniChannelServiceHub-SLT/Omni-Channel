7e0b1a5872c52d0550edccd13d36118c780d78bb
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-08-29 11:24:12 +0530

    Add Data Gift Packages API endpoints
    Introduces model, controller, and routes for Data Gift Packages, allowing clients to list available data gift packages for a subscriber. Integrates the new routes into the main app.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const promotionRoutes = require('./BBVAS/BonusData/routes/promotionRoutes');
+
+const dataGiftRoutes = require("./BBVAS/DatagiftPackages/routes/dataGiftRoutes");
+const promotionRoutes = require("./BBVAS/BonusData/routes/promotionRoutes");
 // const accountRoutes = require('./routes/account.routes');
 
 // Middleware
@@ -8,12 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/tmf-api/promotionManagement/v4/promotion', promotionRoutes);
+app.use("/tmf-api/promotionManagement/v4/promotion", promotionRoutes);
+app.use("/tmf-api/usageManagement/v4/DataGiftPackages", dataGiftRoutes);
 // app.use('/api/Account', accountRoutes);
 
 // Health check
-app.get('/', (req, res) => {
-  res.send('Omini API Server is running ✅');
+app.get("/", (req, res) => {
+  res.send("Omini API Server is running ✅");
 });
 
 module.exports = app;
