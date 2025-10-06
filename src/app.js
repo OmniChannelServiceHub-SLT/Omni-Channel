b6555c2ad300766b33643a92c637131dd54358b7
Shaini2003 <shinitharushika2020@gmail.com>
2025-10-06 12:52:34 +0530

    Changes app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Import Routes
+// const promotionRoutes = require('./BBVAS/BonusData/routes/promotionRoutes');
+const billRoutes = require('./EBill/BillDownloadRequest/routes/billDownloadRoutes');
+
+
+
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Replaces body-parser.json()
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
 
 // Routes
-const billRoutes = require('./EBill/BillDownloadRequest/routes/billDownloadRoutes');
 app.use('/tmf-api/customerBillManagement/v5', billRoutes);
 
 module.exports = app; // Export the Express app
