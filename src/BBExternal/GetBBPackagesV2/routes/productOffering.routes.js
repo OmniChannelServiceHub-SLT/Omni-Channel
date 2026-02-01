160fd3cfad2a46321b9b5a4665fade965c9c0701
MuspellGod <tanjaleef@gmail.com>
2026-02-01 18:57:53 +0530

    new files merging done

--- src/BBExternal/GetBBPackagesV2/routes/productOffering.routes.js
+++ src/BBExternal/GetBBPackagesV2/routes/productOffering.routes.js
@@ -2,21 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productOffering.controller');
-<<<<<<< HEAD
 const authMiddleware = require("../../../middleware/authMiddleware");
 
 
 
 // GetBBPackagesV2
 router.get('/', authMiddleware, controller.getBBPackagesV2);
-=======
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.get(
-  '/BBExternal/GetBBPackagesv2',
-  authMiddleware,
-  controller.getBBPackagesV2
-);
->>>>>>> main
 
 module.exports = router;
