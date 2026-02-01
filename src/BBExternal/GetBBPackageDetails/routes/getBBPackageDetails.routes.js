f29c6e9a2047c4e41fbccd14c2a908511727986f
MuspellGod <tanjaleef@gmail.com>
2026-02-01 18:41:57 +0530

    authmiddleware

--- src/BBExternal/GetBBPackageDetails/routes/getBBPackageDetails.routes.js
+++ src/BBExternal/GetBBPackageDetails/routes/getBBPackageDetails.routes.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/getBBPackageDetails.controller');
+const authMiddleware = require("../../../middleware/authMiddleware");
 
 
 
-
-router.get('/', controller.getBBPackageDetails);
+router.get('/', authMiddleware, controller.getBBPackageDetails);
 
 module.exports = router;
