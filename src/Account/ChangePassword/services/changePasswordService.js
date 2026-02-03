298dd58ede4ff9f4f7e76b5958e06683f762e79b
Slt prodigyhub <thejanashehan.com@gmail.com>
2026-02-03 17:45:03 +0530

    Add ChangePasswordService stub
    Introduce a ChangePasswordService class with a static async changePassword method stub that currently throws a not-implemented error. This placeholder marks Part 2 of the feature; the real implementation (verify current password, hash the new password, persist changes) will be added in a subsequent commit.

---
+++ src/Account/ChangePassword/services/changePasswordService.js
@@ -0,0 +1,12 @@
+class ChangePasswordService {
+  /**
+   * Part 2: service stub.
+   * Real implementation (verify current password, hash new, save) will be added in a later part.
+   */
+  static async changePassword(/* params */) {
+    throw new Error("ChangePasswordService.changePassword not implemented yet");
+  }
+}
+
+module.exports = ChangePasswordService;
+
