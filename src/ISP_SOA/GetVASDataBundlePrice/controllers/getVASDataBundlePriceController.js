bd2c1d236f138b1760fc35e1f42ae8ab82aeb9ff
dasunithimasha <dasunithimasha25@gmail.com>
2026-06-12 18:08:44 +0530

    add controller

---
+++ src/ISP_SOA/GetVASDataBundlePrice/controllers/getVASDataBundlePriceController.js
@@ -0,0 +1,67 @@
+const service =
+require("../services/getVASDataBundlePriceService");
+
+exports.getVASDataBundlePrice =
+async (req, res) => {
+
+    try {
+
+        const { id } = req.query;
+
+        if(!id){
+
+            return res.status(400).json({
+
+                "@type":"Error",
+                code:"400",
+                reason:"Package id is required"
+
+            });
+
+        }
+
+        const packageData =
+        await service.getVASDataBundlePrice(id);
+
+        if(!packageData){
+
+            return res.status(404).json({
+
+                "@type":"Error",
+                code:"404",
+                reason:"Package not found"
+
+            });
+
+        }
+
+        res.status(200).json({
+
+            id: packageData.id,
+
+            name: packageData.name,
+
+            description:
+            packageData.description,
+
+            price:
+            packageData.productOfferingPrice,
+
+            "@type":"ProductOffering"
+
+        });
+
+    }
+    catch(error){
+
+        res.status(500).json({
+
+            "@type":"Error",
+            code:"500",
+            reason:error.message
+
+        });
+
+    }
+
+};
