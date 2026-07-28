03ce392678ba827722f58ef4ca921b6839ab5544
dasunithimasha <dasunithimasha25@gmail.com>
2026-07-29 00:37:26 +0530

    remove lines

--- src/app.js
+++ src/app.js
@@ -289,10 +289,7 @@ app.use("/tmf-api/productOrdering/v4/productOrder", productOrderRoutes);
 app.use("/tmf-api/dataGift/v1", validateDataGiftRoutes);
 app.use("/", vasRoutes);
 app.use("/tmf-api/ServiceActivationAndConfiguration/v4", serviceRoutes);
-app.use(
-  "/tmf-api/productOrdering/v4",
-  require("./BBVAS/DataGiftEnroll/routes/dataGiftEnroll.routes")
-);
+app.use("/tmf-api/productOrdering/v4",require("./BBVAS/DataGiftEnroll/routes/dataGiftEnroll.routes"));
 app.use("/tmf-api/usageManagement/v4", usageRoutes);
 app.use("/tmf-api", contactRoutes);
 app.use("/tmf-api/reportManagement/v5", reportTimePeriodRoutes);
