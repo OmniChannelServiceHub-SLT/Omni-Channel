185b60624ded5b248e0a2847658052462eb09b2f
dasunithimasha <dasunithimasha25@gmail.com>
2026-07-28 23:18:34 +0530

    Revert "update dashboardController.js"
    This reverts commit fb3263daf0e82b63c26dc55fcb1acb65f8cb2139.

--- src/Dashboard/GetExtraGBDashboard/controllers/dashboardController.js
+++ src/Dashboard/GetExtraGBDashboard/controllers/dashboardController.js
@@ -5,15 +5,22 @@ exports.getExtraGBDashboard =
 async (req,res) => {
 
     try {
+        
         const data =
         await dashboardService
         .getExtraGBDashboard();
+
         res.status(200).json(data);
+
     }
     catch(error){
+
         res.status(500).json({
+
             "@type":"Error",
+
             code:"500",
+
             reason:error.message
 
         });
